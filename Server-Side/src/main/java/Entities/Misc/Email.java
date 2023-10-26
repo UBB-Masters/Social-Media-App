f2ecf96464b1c965dc08e3ba9ceabd46020a339c
Bogdan Oprisiu <bobo.oprisiu@gmail.com>
2023-10-26 23:52:25 +0300

    Implement Email Class

--- Server-Side/src/main/java/Entities/Misc/Email.java
+++ Server-Side/src/main/java/Entities/Misc/Email.java
@@ -1,5 +1,34 @@
 package Entities.Misc;
 
-public class Email {
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
+public class Email {
+    private String address;
+
+    public Email(String address) {
+        if (isValidEmail(address))
+            this.address = address;
+        else
+            throw new IllegalArgumentException("Invalid email address");
+    }
+
+    private boolean isValidEmail(String address) {
+        String emailRegex = "^[A-Za-z0-9+_.-]+@(.+)$";
+        Pattern pattern = Pattern.compile(emailRegex);
+        Matcher matcher = pattern.matcher(address);
+        return matcher.matches();
+    }
+
+    public String getEmail() {
+        return address;
+    }
+
+    public void setEmail(String address) {
+        if (isValidEmail(address)) {
+            this.address = address;
+        } else {
+            throw new IllegalArgumentException("Invalid email address");
+        }
+    }
 }
